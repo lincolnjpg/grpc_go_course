15dcfd43f5f8f357b779c546497fd3711ae04133
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2023-09-19 02:34:48 -0300

    feat: add greet server's code

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	pb "github.com/lincolnjpg/grpc_go_course/greet/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr = "0.0.0.0:50051"
+
+func main() {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln("Could not listen", err)
+		return
+	}
+
+	log.Printf("Listenin on %v", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(listener); err != nil {
+		log.Fatalln("Could not serve", err)
+	}
+}
