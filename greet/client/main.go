3ce2f45543e5d494479b38a1b429eec12bd0f878
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2023-09-19 02:46:47 -0300

    feat: add insecure option to skip tls in greet client's code

--- greet/client/main.go
+++ greet/client/main.go
@@ -4,12 +4,13 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr = "0.0.0.0:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr)
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Could not connect: %v\n", err)
 	}
