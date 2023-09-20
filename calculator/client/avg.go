58ddab83b887691724189fb778556634b3ab53a3
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2023-09-20 02:02:33 -0300

    refactor: use int array instead of AvgRequest array

--- calculator/client/avg.go
+++ calculator/client/avg.go
@@ -10,25 +10,19 @@ import (
 
 func doAvg(c pb.CalculatorServiceClient) {
 	log.Println("doLongGreet was invoked")
-	reqs := []*pb.AvgRequest{
-		{
-			Num: 7,
-		},
-		{
-			Num: 12,
-		},
-		{
-			Num: 3,
-		},
-	}
+
 	stream, err := c.Avg(context.Background())
 	if err != nil {
 		log.Fatalf("Error while calling Avg: %v\n", err)
 	}
 
-	for _, req := range reqs {
-		log.Printf("Sending req: %v\n", req)
-		stream.Send(req)
+	numbers := []int64{7, 12, 4}
+
+	for _, number := range numbers {
+		log.Printf("Sending req: %v\n", number)
+		stream.Send(&pb.AvgRequest{
+			Num: number,
+		})
 		time.Sleep(1 * time.Second)
 	}
 
