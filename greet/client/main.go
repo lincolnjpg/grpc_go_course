8e93c9c50e85bd8833806d97bdfa579f269e8e19
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2023-09-20 04:22:25 -0300

    security: adding TLS

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,25 +2,37 @@ package main
 
 import (
 	"log"
-	"time"
 
 	pb "github.com/lincolnjpg/grpc_go_course/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
-var addr = "0.0.0.0:50051"
+var addr = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+		if err != nil {
+			log.Fatalf("Error while load CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		log.Fatalf("Could not connect: %v\n", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreetServiceClient(conn)
-	//doGreet(c)
+	doGreet(c)
 	//doGreetManyTimes(c)
 	//doLongGreet(c)
 	//doGreetEveryone(c)
-	doGreetWithDeadline(c, 1*time.Second)
+	//doGreetWithDeadline(c, 1*time.Second)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	pb "github.com/lincolnjpg/grpc_go_course/greet/proto"
-	"google.golang.org/grpc"
 	"log"
 	"net"
+
+	pb "github.com/lincolnjpg/grpc_go_course/greet/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type Server struct {
 	pb.GreetServiceServer
 }
 
-var addr = "0.0.0.0:50051"
+var addr = "localhost:50051"
 
 func main() {
 	listener, err := net.Listen("tcp", addr)
@@ -22,7 +24,21 @@ func main() {
 
 	log.Printf("Listenin on %v", addr)
 
-	s := grpc.NewServer()
+	opts := []grpc.ServerOption{}
+	tls := true
+
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed to server: %v\n", err)
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 
 	if err = s.Serve(listener); err != nil {
